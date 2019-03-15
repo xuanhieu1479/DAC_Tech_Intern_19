c78ef9901d8c6fa71ad42509349661e8313942e4
Hieu <xuanhieu1479@gmail.com>
2019-03-15 11:12:29 +0700

    Fix fail to register on heroku bug Here is the problem: Everything runs perfectly on local with mysql However it's unable to do register when deployed on heroku with postgres instead mysql So adding  = 'user_name' to model did the trick But somehow it caused login stops functioning Turn out I need to disable auto_increment for primaryKey as well Lesson learned -> test on deployed environment not on local

--- app/User.php
+++ app/User.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    protected $primaryKey = 'user_name';
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
